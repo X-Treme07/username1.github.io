901f92a50184ee89cc892db25eac57af40520faf
X-Treme07 <101004519+X-Treme07@users.noreply.github.com>
2023-02-15 19:38:03 +0530

    Update files.js

--- files.js
+++ files.js
@@ -1,97 +1,79 @@
-// Get form and table elements
 const form = document.getElementById('registration-form');
-const table = document.getElementById('registration-entries');
 
-// Get error message div element
-const errorMessage = document.getElementById('error-message');
+const tableBody = document.querySelector('#registration-table tbody');
 
-// Get array of existing entries from local storage or initialize it if it does not exist
-let entries = JSON.parse(localStorage.getItem('entries')) || [];
+const data = JSON.parse(localStorage.getItem('formData')) || [];
 
-// Populate table with existing entries
-populateTable();
+function renderTableRows(data) {
+  tableBody.innerHTML = '';
+  for (let i = 0; i < data.length; i++) {
+    const row = document.createElement('tr');
+    const name = document.createElement('td');
+    name.textContent = data[i].name;
+    row.appendChild(name);
+    const email = document.createElement('td');
+    email.textContent = data[i].email;
+    row.appendChild(email);
+    const password = document.createElement('td');
+    password.textContent = data[i].password;
+    row.appendChild(password);
+    const dob = document.createElement('td');
+    dob.textContent = data[i].dob;
+    row.appendChild(dob);
+    const acceptedTerms = document.createElement('td');
+    acceptedTerms.textContent = data[i].acceptedTerms;
+    row.appendChild(acceptedTerms);
+    tableBody.appendChild(row);
+  }
+}
 
-// Add event listener to form for submission
+renderTableRows(data);
+
 form.addEventListener('submit', function(event) {
-  // Prevent default form submission behavior
   event.preventDefault();
-
-  // Get form values
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  const dob = new Date(document.getElementById('dob').value);
+  const dob = document.getElementById('dob').value;
   const acceptedTerms = document.getElementById('accepted-terms').checked;
 
-  // Validate email address
   if (!validateEmail(email)) {
-    errorMessage.textContent = 'Invalid email address';
+    alert('Invalid email address');
     return;
   }
 
-  // Validate age
   const age = calculateAge(dob);
   if (age < 18 || age > 55) {
-    errorMessage.textContent = 'Age must be between 18 and 55';
+    alert('You must be between 18 and 55 years old');
     return;
   }
 
-  // Create new entry object
-  const entry = {
+  const newRow = {
     name: name,
     email: email,
     password: password,
-    dob: dob.toLocaleDateString(),
+    dob: dob,
     acceptedTerms: acceptedTerms
   };
+  data.push(newRow);
 
-  // Add new entry to entries array
-  entries.push(entry);
+  localStorage.setItem('formData', JSON.stringify(data));
 
-  // Save updated entries array to local storage
-  localStorage.setItem('entries', JSON.stringify(entries));
+  renderTableRows(data);
 
-  // Clear form
   form.reset();
-
-  // Clear error message
-  errorMessage.textContent = '';
-
-  // Add new row to table
-  addRowToTable(entry);
 });
 
-// Function to validate email address
 function validateEmail(email) {
-  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
 
-// Function to calculate age from date of birth
-function calculateAge(dob) {
+function calculateAge(dateOfBirth) {
+  const dob = new Date(dateOfBirth);
   const diff = Date.now() - dob.getTime();
   const ageDate = new Date(diff);
   return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-// Function to add row to table
-function addRowToTable(entry) {
-  const row = table.insertRow(-1);
-  const nameCell = row.insertCell(0);
-  const emailCell = row.insertCell(1);
-  const passwordCell = row.insertCell(2);
-  const dobCell = row.insertCell(3);
-  const acceptedTermsCell = row.insertCell(4);
-  nameCell.textContent = entry.name;
-  emailCell.textContent = entry.email;
-  passwordCell.textContent = entry.password;
-  dobCell.textContent = entry.dob;
-  acceptedTermsCell.textContent = entry.acceptedTerms ? 'Yes' : 'No';
-}
-
-// Function to populate table with existing entries
-function populateTable() {
-  for (const entry of entries) {
-    addRowToTable(entry);
-  }
 }
